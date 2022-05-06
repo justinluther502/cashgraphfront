df6da801f01737bbcbf16dde812f7bda2a118f2e
Justin Luther <justinluther@lutherwealth.com>
2022-05-06 19:20:46 -0400

    built out optimizer dashboard.

---
+++ src/components/OptimizerTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-simple-table dense>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th class="text-left">
+          Setting
+        </th>
+        <th class="text-left">
+          Current Selection
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Portfolio Flavor</td>
+        <td>
+          {{ flavors.filter(choice => choice.id == flavor_id)[0].label }}
+        </td>
+      </tr>
+      <tr>
+        <td>Risk Measure</td>
+        <td>
+          {{ risks.filter(measure => measure.name == risk)[0].label }}
+        </td>
+      </tr>
+      <tr>
+        <td>Constraints used?</td>
+        <td>No</td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+  import { get } from 'vuex-pathify'
+  export default {
+    name: "OptimizerTable.vue",
+    computed: {
+      flavor_id: get('flavors/selected_flavor_id'),
+      flavors: get('flavors/flavors'),
+      risk: get('mpt_params/selected_risk'),
+      risks: get('mpt_params/risk_measures'),
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,25 +1,79 @@
 <template>
   <div>
-    <view-header :title="title" :title_prefix="title_prefix" />
-    <frontier-spark/>
+    <view-header :title="title" :title_prefix="title_prefix"/>
+    <v-row>
+      <v-col md="3">
+        <v-card class="fill-height">
+          <v-card-title>
+            Optimizer Selections
+          </v-card-title>
+          <optimizer-table/>
+          <v-btn
+            class="mx-2 mb-10"
+            fab
+            dark
+            large
+            color="purple"
+            absolute
+            bottom
+            @click="runOptimizer"
+          >
+            <v-icon dark>
+              mdi-play
+            </v-icon>
+          </v-btn>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            Efficient Frontier
+          </v-card-title>
+          <frontier-spark/>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            Portfolio Weights
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            Selected Portfolio Composition
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
   import viewHeader from "@/components/ViewHeader"
   import frontierSpark from "@/components/FrontierSpark"
+  import OptimizerTable from "@/components/OptimizerTable"
 
   export default {
     name: "DashboardView.vue",
     components: {
       viewHeader,
       frontierSpark,
+      OptimizerTable,
     },
     data() {
       return {
         title: "Dashboard",
         title_prefix: "Modern Portfolio Theory",
       }
+    },
+    methods: {
+      runOptimizer() {
+        console.log("runOptimizer")
+      },
     }
   }
 </script>
