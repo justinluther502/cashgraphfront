480f46f8c5716b39e1b597381fa76271eb25a87c
Justin Luther <justinluther@lutherwealth.com>
2022-05-08 13:33:06 -0400

    pie chart done.

--- src/components/PieFrame.vue
+++ src/components/PieFrame.vue
@@ -1,17 +1,27 @@
 <template>
-  <p>
-    {{ weights_array.map((e, i) => e[slice]) }}
-  </p>
+  <span>
+    <port-pie-chart
+      :asset_labels="asset_labels"
+      :asset_data="weights_array.map((e, i) => e[slice])"
+      :key="slice"
+    >
+    </port-pie-chart>
+  </span>
 </template>
 
 <script>
   import { get } from 'vuex-pathify'
+  import PortPieChart from "@/components/PortPieChart"
 
   export default {
     name: "PieFrame.vue",
+    components: {
+      PortPieChart
+    },
     computed: {
       weights_array: get('frontier/portfolio_weights'),
       slice: get('frontier/slice'),
+      asset_labels: get('frontier/asset_labels'),
     }
   }
 </script>
---
+++ src/components/PortPieChart.vue
@@ -0,0 +1,101 @@
+<template>
+  <Pie
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script>
+  import {Pie} from 'vue-chartjs/legacy'
+
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    CategoryScale,
+  } from 'chart.js'
+
+  ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+
+  export default {
+    name: 'PortPieChart',
+    components: {
+      Pie,
+    },
+    props: {
+      chartId: {
+        type: String,
+        default: 'pie-chart',
+      },
+      datasetIdKey: {
+        type: String,
+        default: 'label',
+      },
+      width: {
+        type: Number,
+        default: 400,
+      },
+      height: {
+        type: Number,
+        default: 400,
+      },
+      cssClasses: {
+        default: '',
+        type: String,
+      },
+      styles: {
+        type: Object,
+        default: () => {
+        },
+      },
+      plugins: {
+        type: Array,
+        default: () => [],
+      },
+      asset_labels: Array,
+      asset_data: Array,
+    },
+    data() {
+      return {
+        chartData: {
+          labels: this.asset_labels,
+          datasets: [
+            {
+              backgroundColor: [
+                '#dd0090',
+                '#fc0af1',
+                '#093bba',
+                '#1172ca',
+                '#87f0aa',
+                '#72ed7f',
+                '#32e339',
+                '#e0fc52',
+                '#f4cc65',
+                '#aa5e21',
+              ],
+              data: this.asset_data,
+              datalabels: {
+                labels: {
+                  title: null,
+                },
+              },
+            },
+          ],
+        },
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false,
+        },
+      }
+    },
+  }
+</script>
