9041892eca07fd7631b7098a7b7b51cb578bc5cb
Justin Luther <justinluther@gmail.com>
2022-07-03 09:00:58 -0400

    added some aws stuff

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 
-const endpoint = process.env.VUE_APP_BASE_PATH
+const endpoint = process.env.VUE_APP_BASE_PATH_LAMBDA
+// fake comment because I changed base path
 
 const apiCall = ({ url, data, method }) =>
   axios({
