1088d7b301a302054952eb4baf59b1372b0aba8d
Justin Luther <justinluther@gmail.com>
2022-07-04 11:16:50 -0400

    configured for new api endpoint.

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 
-const endpoint = process.env.VUE_APP_BASE_PATH_LAMBDA
+const endpoint = process.env.VUE_APP_BASE_PATH_API
 // fake comment because I changed base path
 
 const apiCall = ({ url, data, method }) =>
