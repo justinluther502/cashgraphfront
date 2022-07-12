b16981bb0a615e59ad7b6391eb59cb8ec2fb3b3c
Justin Luther <justinluther@gmail.com>
2022-07-11 22:27:34 -0400

    skeleton for about section complete

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <view-header :title="title" :title_prefix="title_prefix"/>
-    <v-row v-for="section in sections" :key="section.id">
-      <v-col cols="12" md="6" v-if="section.id % 2 !==0">
-        <v-img
-          :src="section.image_src"
-          max-height="300"
-        />
-      </v-col>
+
+    <!--Show it as L/R alternating on large screens-->
+    <div class="d-none d-md-block">
+      <v-row v-for="section in sections" :key="section.id">
+        <v-col cols="12" md="6" v-if="section.id % 2 !==0">
+          <v-img
+            :src="require('@/assets/' + section.image_src)"
+            max-height="300"
+          />
+        </v-col>
         <v-col cols="12" md="6">
           <h2>{{ section.header }}</h2>
           <p v-for="paragraph in section.paragraphs" :key="paragraph">
@@ -16,11 +19,30 @@
         </v-col>
         <v-col cols="12" md="6" v-if="section.id % 2 === 0">
           <v-img
-            :src="section.image_src"
+            :src="require('@/assets/' + section.image_src)"
             max-height="300"
           />
         </v-col>
-    </v-row>
+      </v-row>
+    </div>
+
+    <!--Show it as a single column alternating text/photo on smaller screens-->
+    <div class="d-md-none">
+      <v-row v-for="section in sections" :key="section.id">
+        <v-col cols="12" md="6">
+          <h2>{{ section.header }}</h2>
+          <p v-for="paragraph in section.paragraphs" :key="paragraph">
+            {{ paragraph }}
+          </p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-img
+            :src="require('@/assets/' + section.image_src)"
+            max-height="300"
+          />
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -38,10 +60,10 @@ export default {
   },
   data() {
     return {
-      title: "About " + this.$webapptitle,
+      title: "So what exactly is going on under the hood?",
       title_prefix: "About " + this.$webapptitle,
     }
-  }
+  },
 }
 </script>
 
