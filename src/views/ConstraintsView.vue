99a497404f1f3ee9b9b998b43d1f34f34ace4aec
Justin Luther <justinluther@gmail.com>
2022-07-15 16:37:36 -0400

    added global constraint range slider.

--- src/views/ConstraintsView.vue
+++ src/views/ConstraintsView.vue
@@ -14,28 +14,31 @@
     </v-row>
     <v-row>
       <v-col cols="12" md="6">
-        <v-card class="mb-6" elevation="8">
+        <v-card class="mb-6 py-0" elevation="8">
           <v-card-title>
             Global Constraints
           </v-card-title>
           <v-card-text>
             <v-row>
-              <v-col>
-                Minimum
-              </v-col>
-              <v-col>
-                0.0
-              </v-col>
-              <v-col>
-                Maximum
-              </v-col>
-              <v-col>
-                1.0
+              <v-col class="col-4 py-0"/>
+              <v-col class="col-8 py-0">
+                <v-range-slider
+                  min="0.0"
+                  max="1.0"
+                  thumb-label="always"
+                  step="0.01"
+                  v-model="glob_constraints"
+                  @change="globalSetConstraints"
+                >
+                  <template v-slot:thumb-label="props">
+                    {{ (props.value * 100).toFixed(0) }}%
+                  </template>
+                </v-range-slider>
               </v-col>
             </v-row>
           </v-card-text>
         </v-card>
-        <v-card elevation="8">
+        <v-card elevation="8" :key="constraint_key">
           <v-card-title>
             Constraints
           </v-card-title>
@@ -44,7 +47,7 @@
               v-for="asset, index in flavors[selection]['assets']"
               :key="asset"
             >
-              <v-col class="col-4">{{asset}}</v-col>
+              <v-col class="col-4">{{ asset }}</v-col>
               <v-col class="col-8">
                 <v-range-slider
                   min="0.0"
@@ -53,9 +56,9 @@
                   step="0.01"
                   v-model="constraints[index]"
                 >
-                <template v-slot:thumb-label="props">
-                  {{ (props.value * 100).toFixed(0) }}%
-                </template>
+                  <template v-slot:thumb-label="props">
+                    {{ (props.value * 100).toFixed(0) }}%
+                  </template>
                 </v-range-slider>
               </v-col>
             </v-row>
@@ -83,8 +86,10 @@ export default {
     return {
       title: "Constraints",
       title_prefix: "Portfolio weight constraints",
+      glob_constraints: [0.0, 1.0],
       constraints: [],
       range_vals: [0.0, 1.0],
+      constraint_key: 0,
       // range_rules: [v => v[1] - v[0] >= 0.1 ||
       //   "Leave a wider range for this asset or things will get weird"]
     }
@@ -109,6 +114,12 @@ export default {
           this.constraints[i][1]
       }
     },
+    globalSetConstraints() {
+      for (let i = 0; i < this.constraints.length; i++) {
+        this.constraints[i] = this.glob_constraints
+      }
+      this.constraint_key ++
+    }
   }
 }
 </script>
